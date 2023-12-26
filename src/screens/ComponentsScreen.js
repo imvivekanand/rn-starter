652ca6116eb4ff1270e3653e9d9a5383c8655bf9
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-12-26 18:30:23 +0530

    Learnt about components

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-  return (
-    <div>
-      <Text style={styles.textStyle}>Component Screen 1</Text>;
-    </div>
-  )
+    const greeting = ["Hi there!", 123];
+    const devDetails = {
+        name: "Vivek Anand",
+        age: 26
+    };
+    const jsxVariable = <Text>I'm a JSX Variable</Text>
+
+
+    return <View>
+        <Text style={styles.textStyle}>Component Screen 1</Text>
+        <Text>{greeting}</Text>
+        <Text style={styles.textXl}>{devDetails.name}</Text>
+        <Text>{devDetails.age}</Text>
+        {jsxVariable}
+    </View>
 }
 
 const styles = StyleSheet.create({
     textStyle: {
         fontSize: 30,
-
+    },
+    textXl:{
+        fontSize: 45,
     }
 })
 
