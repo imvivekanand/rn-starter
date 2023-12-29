24c9dbcb69be9b116f07d957795fa9afe525dd6a
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-12-29 23:08:45 +0530

    list updated

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,16 +1,46 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, FlatList} from 'react-native'
 
 const ListScreen = () => {
+  const friends = [
+
+    // Method1: Without keyExtractor
+    // {name: 'Friend #1', key: '1'},
+    // {name: 'Friend #2', key: '2'},
+    // {name: 'Friend #3', key: '3'},
+    // {name: 'Friend #4', key: '4'},
+    // {name: 'Friend #5', key: '5'},
+    // {name: 'Friend #6', key: '6'},
+    // {name: 'Friend #7', key: '7'},
+    // {name: 'Friend #8', key: '8'},
+    // {name: 'Friend #9', key: '9'},
+
+    {name: 'Friend #1', age: 'Age 20' },
+    {name: 'Friend #2', age: 'Age 45'},
+    {name: 'Friend #3', age: 'Age 32'},
+    {name: 'Friend #4', age: 'Age 27'},
+    {name: 'Friend #5', age: 'Age 53'},
+    {name: 'Friend #6', age: 'Age 30'},
+    {name: 'Friend #7', age: 'Age 45'},
+    {name: 'Friend #8', age: 'Age 29'},
+    {name: 'Friend #9', age: 'Age 24'},
+  ]
   return (
-    <View>
-      
-    </View>
+    <FlatList 
+    // horizontal
+    // showsHorizontalScrollIndicator = {false}
+    keyExtractor={(friend) => friend.name}
+    data={friends} 
+    renderItem={({item})=>{
+      return <Text style={styles.textStyle}>{item.name} - {item.age}</Text>
+    }}/>
   )
 }
 
 const styles = StyleSheet.create({
-    
+    textStyle: {
+      marginVertical: 50
+    }
 })
 
 export default ListScreen
